26177a298e7ff1b9bc9a3a9f4596dab721e32b3b
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2024-03-07 00:53:35 +0300

    Добавление файлов фронтенда

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <PhotoEditor />
     </v-main>
   </v-app>
 </template>
---
+++ client/src/components/PhotoEditor.vue
@@ -0,0 +1,26 @@
+<template>
+  <tui-image-editor :include-ui="useDefaultUI" :options="options"></tui-image-editor>
+</template>
+
+<script>
+import 'tui-color-picker/dist/tui-color-picker.css';
+import 'tui-image-editor/dist/tui-image-editor.css';
+import ImageEditor from '@toast-ui/vue-image-editor/src/ImageEditor.vue';
+
+
+export default {
+  components: {
+    'tui-image-editor': ImageEditor,
+  },
+  data() {
+    return {
+      useDefaultUI: true,
+      options: {
+        // for tui-image-editor component's "options" prop
+        cssMaxWidth: 700,
+        cssMaxHeight: 500,
+      },
+    };
+  },
+};
+</script>
